ffed0ae890a8ac8648ce40cad2f166a911859796
Joaobazzo <joao.bazzo@gmail.com>
2022-05-31 14:45:57 -0300

    update razao_pop

--- R/1.5_razao_pop.R
+++ R/1.5_razao_pop.R
@@ -11,10 +11,12 @@ library(patchwork)
 # add style
 source('R/colours.R')
 
-# Read files ------
+# Read file ------
 
-tmp_join_bus_raw <- readr::read_rds("data/socio_acc-all_2019.rds") 
-tmp_join <- data.table::copy(tmp_join_bus_raw) %>%  
+tmp_join_raw <- readr::read_rds("data/socio_acc-all_2019.rds")  # source("R/0.0_join_acess-soc.R")
+
+# initial filter
+tmp_join <- data.table::copy(tmp_join_raw) %>%  
   .[pop_total > 0,] %>% 
   .[mode == "walk",] %>% 
   .[pico == 1,] %>% 
@@ -22,45 +24,57 @@ tmp_join <- data.table::copy(tmp_join_bus_raw) %>%
   .[decil %in% c(1:2),decil_status := "Pobre"]
 
 
-#tmp_join[cor_negra  > 0.5 * (pop_total),dummyCor := "Negra"]
-#tmp_join[cor_branca  > 0.5 * (pop_total),dummyCor := "Branca"]
-tmp_join[decil %in% c(1:2)  & cor_negra   > 0.5 * (pop_total),dummySoc := "Baixa Negra" ]
-tmp_join[decil %in% c(9:10) & cor_negra   > 0.5 * (pop_total),dummySoc := "Alta Negra"  ]
-tmp_join[decil %in% c(1:2)  & cor_branca  > 0.5 * (pop_total),dummySoc := "Baixa Branca"]
-tmp_join[decil %in% c(9:10) & cor_branca  > 0.5 * (pop_total),dummySoc := "Alta Branca" ]
+# organize data in columns
+tmp_join <- data.table::melt(tmp_join
+                             ,measure.vars = list(c("cor_branca","cor_negra"
+                                                    ,"cor_amarela","cor_indigena"))
+                             ,variable.name = "cor"
+                             ,value.name = "total_by_color")
 
-tmp_join[dummySoc == "Alta Negra" & city == "spo",]
-#tmp_melt <- data.table::melt(tmp_join
-#                             ,measure.vars = list(c("cor_branca","cor_negra"
-#                                                    ,"cor_amarela","cor_indigena"))
-#                             ,variable.name = "cor"
-#                             ,value.name = "total_by_color")
-
-tmp_join[origin == "89a88cd814fffff"]
-tmp_join[dummySoc == "Alta Negra" & city == "spo",]
-# by (city,mode,pico,ano,cor)
-tmp_w1 <- data.table::copy(tmp_join) %>% 
+# sum population by (city,mode,pico,ano,cor)
+tmp_join <- tmp_join %>% 
   .[,
     lapply(.SD,sum,na.rm = TRUE)
-    ,by = .(city,dummySoc)
-    ,.SDcols = c("cor_branca","cor_negra","pop_total")
+    ,by = .(city,decil_status,cor)
+    ,.SDcols = c("total_by_color")
   ]
 
-tmp_w1[city == "spo"]
-#tmp_w1 <- tmp_w1[!is.na(decil_status),]
-tmp_w1 <- tmp_w1[!is.na(dummySoc),]
-tmp_w1[,razao_racial := cor_branca / cor_negra]
-tmp_w1[,razao_racial_adj := razao_racial / min(razao_racial),by = city]
+# remove unecessary data
+tmp_join <- tmp_join %>% 
+  .[!is.na(decil_status),] %>% 
+  .[cor %in% c("cor_branca","cor_negra"),]
 
-tmp_w1[city == "cam"]
+# add negra_pobre into a column
+tmp_join <- tmp_join[,{
+  
+  tmp_id <- which(decil_status == "Pobre" & cor == "cor_negra")
+  
+  list(
+    cor
+    , decil_status
+    , total_by_color
+    , negra_pobre =  total_by_color[tmp_id]
+  )
+  
+},by = city]
 
-# plot
-tmp_city_order <- data.table::copy(tmp_w1) %>% 
-  #.[decil_status  == "Pobre"] %>% 
-  .[dummySoc   == "Alta Branca"] %>% 
-  .[order(razao_racial_adj),city]
+
+# verify data
+tmp_join[city == "spo"]
+
+# razao racial
+tmp_join[,razao_racial := negra_pobre / total_by_color]
+
+tmp_join[city == "cam"]
+
+# plot ------
+
+# find city order
+tmp_city_order <- data.table::copy(tmp_join) %>% 
+  .[decil_status  == "Pobre" & cor == "cor_branca"] %>% 
+  .[order(razao_racial),city]
+
 tmp_city_order
-
 dt_tmp_city <- data.table(levels = c("bel","bho","bsb","cam","cgr","cur","duq"
                                      ,"for","goi","gua","mac","man","nat","poa"
                                      ,"rec","rio","sal","sgo","slz","spo"),
@@ -71,60 +85,49 @@ dt_tmp_city <- data.table(levels = c("bel","bho","bsb","cam","cgr","cur","duq"
                                      ,"Recife","Rio de Janeiro","Salvador"
                                      ,"São Gonçalo"
                                      ,"São Luís","São Paulo"))
+
+# add as factors
 tmp_city_labels <- dt_tmp_city[order(match(levels,tmp_city_order)),] %>% 
   .[1:length(tmp_city_order),labels]
 
-tmp_w1[,city_f := factor(city
-                           ,levels = tmp_city_order
-                           ,labels = tmp_city_labels)]
+tmp_join[,city_f := factor(
+   x = city
+  ,levels = tmp_city_order
+  ,labels = tmp_city_labels
+)]
 
-tmp_w1[city=="spo"]
+# add label
+tmp_join[cor == "cor_branca" & decil_status == "Rica" , label := "Alta Branca" ]
+tmp_join[cor == "cor_branca" & decil_status == "Pobre", label := "Baixa Branca"]
+tmp_join[cor == "cor_negra"  & decil_status == "Rica" , label := "Alta Negra"  ]
+tmp_join[cor == "cor_negra"  & decil_status == "Pobre", label := "Baixa Negra" ]
+
+# verify data for spo
+tmp_join[city == "spo"]
+
+
 # plot
-
 ggplot() + 
-  geom_point(data = tmp_w1
-             ,aes(x = razao_racial_adj,y = city_f
-                  ,color = dummySoc )
-             ,size=2.5
-             ,alpha = 1,shape = 1,stroke = 2.)+
-  scale_colour_aop(palette = "clevel")+
+  # add points
+  geom_point(data = tmp_join
+             ,aes(x = razao_racial,y = city_f
+                  ,color = label)
+             ,size=2.5,alpha = 1
+             ,shape = 1,stroke = 2.) +
+  # add pallete and x_continuous
+  scale_colour_aop(palette = "clevel") +
+  scale_x_continuous(labels = 1:8
+                     ,breaks = seq(1
+                                   ,max(round(tmp_join$razao_racial,0))
+                                   ,by = 1)) +
+  # add vertical line
   geom_vline(xintercept = 1,linetype = "dashed")+
-  labs(x = "Razão cor-renda / pop. negra de baixa renda"
+  # labs
+  labs(x = "Razão pop. negra de baixa renda / pop. de cor/raça - renda"
        , y = NULL
-       #, color = "Nível de renda"
-       , color = "Renda / Cor"
+       , color = "Renda - Cor/raça"
        , title = "Razão população conforme cor e renda"
-       , subtitle = "Proporção entre população conforme cor/raça e nível de renda \najustado pela pop. negra de baixa renda")+
-  theme(legend.position = c(0.875,0.2))
-  
-  
-  ggplot() + 
-  geom_segment(data = tmp_plot[,list(min(TMISA),max(TMISA)),by = city_f]
-               ,aes(x = V1,xend = V2,y = city_f,yend = city_f)
-               ,color = "grey")+
-  geom_point(data = tmp_plot[decil_status != "media populacao"]
-             ,aes(x = TMISA,y = city_f
-                  ,color = label),size=2.5,alpha = 1,shape = 1,stroke = 2.)+
-  geom_point(data = tmp_plot[decil_status == "media populacao"]
-             ,aes(x = TMISA,y = city_f)
-             ,size=2.5,shape = 3,alpha = 1,color = "grey")+
-  scale_colour_aop(palette = "clevel")+
-  # facet_grid(rows  = vars(mode),scales = "free")+
-  labs(#title = "Medida cumulativa ativa (2019)"
-    title = NULL
-    ,subtitle = "Tempo mínimo"
-    ,color = "Renda e raça"
-    ,x = "Tempo mínimo (minutos)"
-    , y = NULL
-    , fill = "Cor - Modo de transporte")+
-  #guides(shape = "none")+
-  aop_style()
+       , subtitle = "Proporção entre população negra de baixa renda sobre\npopulação por cor/raça - renda")+
+  # adjust legend.position 
+  theme(legend.position = c(0.875,0.35))
 
-
-TMISA_transit_plot
-
-TMISA_transit_plot + CMASA30_transit_plot +
-  plot_annotation(tag_levels = 'I',tag_prefix = "(",tag_suffix = ")")
-ggsave(filename = "figures/CMASA30_TMI_transit_20-20rico.png",
-       width = 34,height = 20,scale = 0.6
-       ,units = "cm",bg = "white",dpi = 300)
